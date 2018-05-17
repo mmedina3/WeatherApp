ea82a32129ead626532e8f5c41a0875029e06ae4
Michelle Medina <medina.m1016@gmail.com>
2018-05-17 12:22:51 -0700

    fixed bug

--- src/App.js
+++ src/App.js
@@ -13,35 +13,41 @@ class App extends React.Component {
     country: undefined,
     humidity: undefined,
     description: undefined,
-    error: undefined,
+    error: undefined
   }
 
   getWeather = async (e) => {
     e.preventDefault();
-    const city = e.target.elements.city.value;
-    const country = e.target.elements.country.value;
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}, ${country}&appid=${API_KEY}&units=metric`);
-    const data = await api_call.json();
-    if(city && country){
-    console.log(data);
-    this.setState({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      error: ""
+      try {
+        const city = e.target.elements.city.value;
+        const country = e.target.elements.country.value;
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+        const data = await api_call.json();
+    
+    if(city && country) {
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
     });
-  }else{
-    this.setState({
-    temperature: undefined,
-    city: undefined,
-    country: undefined,
-    humidity: undefined,
-    description: undefined,
-    error: "Please enter the values!"
-   });
-  }
+    } else {
+      this.setState({
+      temperature: undefined,
+      city: undefined,
+      country: undefined,
+      humidity: undefined,
+      description: undefined,
+      error: "Please enter the values!"
+     });
+    }
+    } 
+    catch(e) {
+      console.log('Error!', e);
+    }   
 }
 render(){
   return(
